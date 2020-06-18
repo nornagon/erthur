6bacbd470045b1e1098f1a0d3c12cca8f2673ae6
Jeremy Apthorp <nornagon@nornagon.net>
2020-06-17 18:26:10 -0700

    handle non-pot textures

--- draw.js
+++ draw.js
@@ -9,11 +9,8 @@ const fontHeight = 9
 
 console.log("Parsing map...")
 const {width: mapWidthInEmbarkTiles, height: mapHeightInEmbarkTiles, map} = JSON.parse(fs.readFileSync('../map.json'))
-// TODO: ehh.... the map is 129x129 embark tiles, which is hard to convert into
-// leaflet tiles. cheat for now.
-const trueMapWidthInTiles = mapWidthInEmbarkTiles * 16
-const mapWidthInTiles = (mapWidthInEmbarkTiles - 1)* 16
-const mapHeightInTiles = (mapHeightInEmbarkTiles - 1)* 16
+const mapWidthInTiles = mapWidthInEmbarkTiles * 16
+const mapHeightInTiles = mapHeightInEmbarkTiles * 16
 
 const colors = {
   BLACK: 0,
@@ -53,8 +50,7 @@ loadImage(fontFile).then(async (font) => {
   const chunkWidthInPixels = chunkSizeInTiles * fontWidth
   const chunkHeightInPixels = chunkSizeInTiles * fontHeight
 
-  const maxZoom = Math.log2(Math.max(mapWidthInChunks, mapHeightInChunks))
-  if (maxZoom !== Math.round(maxZoom)) throw new Error("don't yet know how to handle non-power-of-two map sizes")
+  const maxZoom = Math.ceil(Math.log2(Math.max(mapWidthInChunks, mapHeightInChunks)))
 
   function chunkFileName(z, x, y) {
     return __dirname + `/tiles/${z}/${x}/${y}.png`
@@ -81,7 +77,9 @@ loadImage(fontFile).then(async (font) => {
       for (let cx = 0; cx < chunkSizeInTiles; cx++) {
         const absX = chunkX * chunkSizeInTiles + cx
         const absY = chunkY * chunkSizeInTiles + cy
-        const tile = map[absY*trueMapWidthInTiles+absX] || {c: 0, f: 0, b: 0, l: 0}
+        if (absX >= mapWidthInTiles || absY >= mapHeightInTiles)
+          continue
+        const tile = map[absY*mapWidthInTiles+absX] || {c: 0, f: 0, b: 0, l: 0}
 
         const char = tile.c
         const charY = (char & 0xf0) >>> 4
@@ -111,23 +109,27 @@ loadImage(fontFile).then(async (font) => {
   async function drawZoomedOutChunk(z, x, y) {
     // 1. read in the 4 chunks we'll be compositing
     const [topLeft, topRight, bottomLeft, bottomRight] = await Promise.all([
-      loadImage(chunkFileName(z + 1, x * 2, y * 2)),
-      loadImage(chunkFileName(z + 1, x * 2 + 1, y * 2)),
-      loadImage(chunkFileName(z + 1, x * 2, y * 2 + 1)),
-      loadImage(chunkFileName(z + 1, x * 2 + 1, y * 2 + 1)),
+      loadImage(chunkFileName(z + 1, x * 2, y * 2)).then(i => i, e => null),
+      loadImage(chunkFileName(z + 1, x * 2 + 1, y * 2)).then(i => i, e => null),
+      loadImage(chunkFileName(z + 1, x * 2, y * 2 + 1)).then(i => i, e => null),
+      loadImage(chunkFileName(z + 1, x * 2 + 1, y * 2 + 1)).then(i => i, e => null),
     ])
     const canvas = createCanvas(chunkWidthInPixels, chunkHeightInPixels)
     const ctx = canvas.getContext('2d')
-    ctx.drawImage(topLeft, 0, 0, chunkWidthInPixels, chunkHeightInPixels, 0, 0, chunkWidthInPixels / 2, chunkHeightInPixels / 2)
-    ctx.drawImage(topRight, 0, 0, chunkWidthInPixels, chunkHeightInPixels, chunkWidthInPixels / 2, 0, chunkWidthInPixels / 2, chunkHeightInPixels / 2)
-    ctx.drawImage(bottomLeft, 0, 0, chunkWidthInPixels, chunkHeightInPixels, 0, chunkHeightInPixels / 2, chunkWidthInPixels / 2, chunkHeightInPixels / 2)
-    ctx.drawImage(bottomRight, 0, 0, chunkWidthInPixels, chunkHeightInPixels, chunkWidthInPixels / 2, chunkHeightInPixels / 2, chunkWidthInPixels / 2, chunkHeightInPixels / 2)
+    if (topLeft)
+      ctx.drawImage(topLeft, 0, 0, chunkWidthInPixels, chunkHeightInPixels, 0, 0, chunkWidthInPixels / 2, chunkHeightInPixels / 2)
+    if (topRight)
+      ctx.drawImage(topRight, 0, 0, chunkWidthInPixels, chunkHeightInPixels, chunkWidthInPixels / 2, 0, chunkWidthInPixels / 2, chunkHeightInPixels / 2)
+    if (bottomLeft)
+      ctx.drawImage(bottomLeft, 0, 0, chunkWidthInPixels, chunkHeightInPixels, 0, chunkHeightInPixels / 2, chunkWidthInPixels / 2, chunkHeightInPixels / 2)
+    if (bottomRight)
+      ctx.drawImage(bottomRight, 0, 0, chunkWidthInPixels, chunkHeightInPixels, chunkWidthInPixels / 2, chunkHeightInPixels / 2, chunkWidthInPixels / 2, chunkHeightInPixels / 2)
     await writeChunk(z, x, y, canvas)
   }
 
   for (let zoomLevel = maxZoom - 1; zoomLevel >= 0; zoomLevel--) {
-    const chunksXInThisZoomLevel = mapWidthInChunks / Math.pow(2, maxZoom - zoomLevel)
-    const chunksYInThisZoomLevel = mapHeightInChunks / Math.pow(2, maxZoom - zoomLevel)
+    const chunksXInThisZoomLevel = Math.ceil(mapWidthInChunks / Math.pow(2, maxZoom - zoomLevel))
+    const chunksYInThisZoomLevel = Math.ceil(mapHeightInChunks / Math.pow(2, maxZoom - zoomLevel))
     for (let chunkY = 0; chunkY < chunksYInThisZoomLevel; chunkY++) {
       for (let chunkX = 0; chunkX < chunksXInThisZoomLevel; chunkX++) {
         await drawZoomedOutChunk(zoomLevel, chunkX, chunkY)
